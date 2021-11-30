38bbadc782abaafb3a4c52e8091bf5c572b2dfac
unknown <andre.campos@ucsm.edu.pe>
2021-11-30 17:25:55 -0500

    Ejercicio 2: Facultades con carreras profesionales

---
+++ Producto.php
@@ -0,0 +1,82 @@
+<?PHP
+if ($_REQUEST['cod']==1){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong> Facultad de Ciencias e Ingenierías Físicas y Formales</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+
+			    <img class="card-img-top border" src="Productos/produ1.jpg" alt="..." "/>
+                            <hr>
+                            <h5><strong>Carreras Profesionales</strong></h5>
+                            <br>
+                            <br>
+                            <br>
+                            <strong>Ingenieria Electronia</strong><br>
+                            <strong>Ingenieria de Industrial</strong><br>
+                            <strong>Ingenieria de Minas</strong><br>
+                            <strong>Ingenieria de Mecanica</strong><br>
+                            <strong>Ingenieria de Sistemas</strong><br>                           
+                            </p>
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+if ($_REQUEST['cod']==2){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong>Facultad de Ciencias y Tecnologías Sociales y Humanidades</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                            <img class="card-img-top border" src="Productos/produ2.jpg" alt="..." />
+                            <hr>
+
+                            <h5><strong>Carreras Profesionales</strong></h5>
+                            <br>
+                            <br>
+                            <br>
+                            <strong>Publicidad y Multimedia</strong><br>
+                            <strong>Comunicacion Social</strong><br>
+                            <strong>Psicologia</strong><br>
+                            <strong>Educacion</strong><br>
+                            <strong>Trabajo Social</strong><br>
+                            <strong>Teologia</strong><br>  
+                            <strong>Turismo y Hoteleria</strong><br>  
+                            </p>
+                                                  
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+if ($_REQUEST['cod']==3){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong>Facultad de Ciencias Farmacéuticas Bioquímicas y Biotecnológicas</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                            <img class="card-img-top border" src="Productos/produ3.jpg" alt="..." />
+                            <hr>
+                            <smallCódigo: A315-57G-53R9</small>
+                            <h5><strong>Carreras Profesionales</strong></h5>
+                            <br>
+                            <br>
+                            <br>
+
+	 		    <strong>Farmacia y Bioquimica</strong><br>
+                            <strong>Ingenieria Biotecnologia</strong><br>
+                                                  
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+
+?>
